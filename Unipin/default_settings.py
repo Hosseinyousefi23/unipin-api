8aa1cf4470bc8cc3c48ec502602a42618965a91c
hossein <hossein.yousefi23@gmail.com>
2019-10-07 16:25:54 +0330

    static root

--- Unipin/default_settings.py
+++ Unipin/default_settings.py
@@ -156,6 +156,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'public/assets')
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = (
     'localhost:4200',
@@ -288,4 +289,3 @@ LOGGING = {
         },
     }
 }
-
